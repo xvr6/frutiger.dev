e1a4dec9916dc80466a62500bd5218c038662f6c
Xavier <xvr0612@gmail.com>
2025-11-11 13:59:56 -0500

    init of new project

---
+++ tasks-app/src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from "vue"; 
+
+const message = ref("Hello World");
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+</style>
